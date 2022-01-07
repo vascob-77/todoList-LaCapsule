8bd718b12c93ef67f97dd4ccdeec36dcc05a7ab6
Vasco <vascbarreiros@gmail.com>
2022-01-07 09:06:31 +0100

    projet ok, bonus a faire

--- script.js
+++ script.js
@@ -1,58 +1,56 @@
-const itemAll = document.querySelectorAll('.item');
-const titre = document.getElementById('titre')
-const categorie = document.getElementById('categorie')
-const date = document.getElementById('date')
-const mainContainer = document.getElementById('main_container');
+const itemAll = document.querySelectorAll(".item");
+const mainContainer = document.getElementById("main_container");
+const titre = document.getElementById("titre");
+const categorie = document.getElementById("categorie");
+const date = document.getElementById("date");
+const btnOk = document.getElementById('ok');
 
-
-let divItem = '';
 
 // console.log(itemAll);
 // ---------- Function ------------
 
-function createElement(nom,balise,classe = '',text='',inBalise){
-    nom = document.createElement(balise);
-    nom.className = classe;
-    nom.textContent = text
-    inBalise.appendChild(nom);
-    divItem = nom;
+const createElement = (balise, container, classe = "", text = "") => {
+    const el = document.createElement(balise);
+    el.className = classe;
+    el.textContent = text;
+    container.appendChild(el);
+    return el;
 }
 
+// Ajout
 
-// Ajout 
+const addItem = (titre,categorie,date) => {
+    const divItem = createElement("div", mainContainer, "item", "");
+    createElement("h3", divItem, "", categorie);
+    createElement("p", divItem, "", titre);
+    createElement("p", divItem, "", date);
 
-const addItem = () => {
+    divItem.addEventListener('click', () => {
+        deleteHandler(divItem)
+    })
+};
 
-    // Div Item
-    createElement('divItem','div','item','',mainContainer)
-    createElement('titleh3','h3','','Ceci est un titre h3',divItem)
-
-    // let titleH3 = document.createElement('h3');
-    // titleH3.textContent = 'Ceci est un titre h3'
-    // divItem.appendChild(titleH3);
-
-}
-
-
-// Supprimer au clic 
+// Supprimer au clic
 
 const deleteHandler = (item) => {
-    item.remove()
-}
-
+    item.remove();
+};
 
 // --------- Evenement -------------
 
-// DELETE CARD 
+// DELETE CARD
 
 itemAll.forEach((item) => {
-    item.addEventListener('click',() => {
-        deleteHandler(item)
-    })
+    item.addEventListener("click", () => {
+        deleteHandler(item);
+    });
+});
+
+// ADD CARD
+
+btnOk.addEventListener('click',() => {
+    addItem(titre.value,categorie.value,date.value)
+    titre.value = '';
+    categorie.value = '';
+    date.value = '';
 })
-
-// ADD CARD 
-
-addItem();
-
-// console.log(itemAll);
